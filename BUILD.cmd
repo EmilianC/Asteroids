8ac5090e66caccdce1f16f7bd352ece8a3d00964
Emilian Cioca <ciocaemilian@hotmail.ca>
2017-06-04 17:15:08 -0400

    Added scripts for building the project

---
+++ BUILD.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+call :BuildConfiguration Debug || goto :eof
+call :BuildConfiguration Release || goto :eof
+
+title Done!
+echo Done!
+goto :eof
+
+:BuildConfiguration
+title Building %1
+echo ^>^>^>^>^>^> Building %1 ^<^<^<^<^<^<
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSbuild.exe" /t:Build /p:Configuration=%1 /property:Platform=Win32 "Asteroids.sln"
+if errorlevel 1 (
+   title Build Failure %1
+   echo ^>^>^>^>^>^> Build Failure %1 ^<^<^<^<^<^<
+   pause
+)
+goto :eof
